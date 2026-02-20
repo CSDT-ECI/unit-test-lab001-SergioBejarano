22c0ab29110d2e68c1e3632147133cbe5f387d32
Sergio Andrés Bejarano Rodríguez <157920809+SergioBejarano@users.noreply.github.com>
2026-02-20 16:04:30 +0000

    test: add unit tests for the crazy_chance function

---
+++ python/test_crazychance.py
@@ -0,0 +1,13 @@
+import pytest
+from yatzy import Yatzy
+
+class TestCrazyChance:
+
+    def test_all_even(self):
+        assert Yatzy.crazy_chance(2, 4, 6, 2, 2) == 48
+
+    def test_all_odd(self):
+        assert Yatzy.crazy_chance(1, 1, 3, 5, 5) == 30
+
+    def test_mixed_even_and_odd(self):
+        assert Yatzy.crazy_chance(2, 4, 3, 5, 6) == 52
